55cff7bb202b2637e180cedc27880ebe2c7f4312
月明星希 <8336454+moon-star-hope@user.noreply.gitee.com>
2020-11-30 15:50:57 +0800

    login背景色--登录盒子--头像区域

---
+++ src/views/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box{
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+    border: 1px solid #ccc;
+    padding: 10px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+    }
+  }
+}
+</style>
